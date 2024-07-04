fdb4b3825775e651a3200366b73a46ef1b8486e1
Ivan Trushin <itrushin99@gmail.com>
2024-07-05 00:43:30 +0300

    [WIP] print something

--- 1/99_hw/tree/main.go
+++ 1/99_hw/tree/main.go
@@ -3,10 +3,65 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
-	"path/filepath"
-	"strings"
 )
+
+const (
+	ISHape    = "│"
+	TShape    = "├"
+	LShape    = "└"
+	DashShape = "─"
+)
+
+func tabify(output io.Writer) io.Writer {
+	fmt.Fprint(output, "\t")
+	return output
+}
+
+func entryInfo(e fs.DirEntry) (string, error) {
+	fi, err := e.Info()
+	if err != nil {
+		return "", err
+	}
+
+	infoString := fi.Name()
+	if !fi.IsDir() {
+		s := fi.Size()
+		if s == 0 {
+			infoString += " " + "(empty)"
+		} else {
+			infoString += " " + fmt.Sprintf("(%db)", s)
+		}
+	}
+	return infoString, nil
+}
+
+func dirTree(output io.Writer, path string, printFiles bool) error {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	for _, e := range entries {
+		if !e.IsDir() && !printFiles {
+			continue
+		}
+		info, err := entryInfo(e)
+		if err != nil {
+			return err
+		}
+		println(info)
+		if e.IsDir() {
+			err = dirTree(tabify(output), path+"/"+e.Name(), printFiles)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
 
 func main() {
 	out := os.Stdout
